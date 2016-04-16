a75445ffcb9c0c43ac9c9e8caf94553196f4df6d
Marvin Liu <marvinliu9@gmail.com>
2016-04-16 18:02:58 +0800

    fix twitter_tests.py

--- feels/twitter_tests.py
+++ feels/twitter_tests.py
@@ -1,6 +1,5 @@
 from TwitterAPI import *
-from vaderSentiment import vaderSentiment
-
+from vaderSentiment.vaderSentiment import sentiment as vader
 
 #authentification
 api = TwitterAPI('BQBZTbY3ugTypaRBq7Is0m6Dh', 'JGeRqs3r42Id4W2Q47NlGwAlNYv0myrBhlUPJeeizQXi56RWBm', auth_type='oAuth2')
@@ -11,6 +10,7 @@ count = 10
 r = api.request('search/tweets', {'q': myQuery, 'lang': 'en', 'count':count})
 
 for item in r: #each tweet will be a Python dictionary
-    print(item['text'].encode('ascii', 'ignore'))
-    vs = vaderSentiment.sentiment("hey w4tasrtgawg")
+    text = item['text'].encode('ascii', 'ignore')
+    print text
+    vs = vader(text)
     print("\n\t" + str(vs))
