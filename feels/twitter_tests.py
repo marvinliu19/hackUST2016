80f972c1dc4ff53ba81d348417c69212442f5737
James Wall <james.wall@wustl.edu>
2016-04-16 17:08:58 +0800

    count variable in tests

--- feels/twitter_tests.py
+++ feels/twitter_tests.py
@@ -7,7 +7,8 @@ api = TwitterAPI('BQBZTbY3ugTypaRBq7Is0m6Dh', 'JGeRqs3r42Id4W2Q47NlGwAlNYv0myrBh
 print("authentification success\n")
 
 myQuery = "Donald Trump"
-r = api.request('search/tweets', {'q': myQuery, 'lang': 'en'})
+count = 10
+r = api.request('search/tweets', {'q': myQuery, 'lang': 'en', 'count':count})
 
 for item in r: #each tweet will be a Python dictionary
     print(item['text'].encode('ascii', 'ignore'))
