3bd6ce918ecc0998da542c7429aa93b4d194fc56
Marvin Liu <marvinliu9@gmail.com>
2016-04-17 04:23:57 +0800

    changed some styles

--- feels/views.py
+++ feels/views.py
@@ -9,62 +9,64 @@ import re
 from .forms import QueryForm
 
 def index(request):
-  form = QueryForm()
-  context = RequestContext(request, {'form': form})
-  return render_to_response('feels/index.html', context)
+    form = QueryForm()
+    context = RequestContext(request, {'form': form})
+    return render_to_response('feels/index.html', context)
 
 def display(request):
-  context = {}
-  bio_output = ""
-  tweets_output = ""
+    context = {}
+    bio_output = ""
+    tweets_output = ""
 
-  name_query = 'kanyewest'
+    name_query = 'kanyewest'
 
-  if request.method == "POST":
-    name_query = request.POST['query_text']
+    if request.method == "POST":
+        name_query = request.POST['query_text']
+        name_query = name_query.replace('@', '')
+    
+    if name_query == '':
+        name_query = 'kanyewest'
 
-  api = TwitterAPI('BQBZTbY3ugTypaRBq7Is0m6Dh', 'JGeRqs3r42Id4W2Q47NlGwAlNYv0myrBhlUPJeeizQXi56RWBm', auth_type='oAuth2')
-  count = 100
-  tweets = api.request('statuses/user_timeline', {'screen_name': name_query, 'count':count, 'exclude_replies':'true', 'include_rts':'false'})
+    api = TwitterAPI('BQBZTbY3ugTypaRBq7Is0m6Dh', 'JGeRqs3r42Id4W2Q47NlGwAlNYv0myrBhlUPJeeizQXi56RWBm', auth_type='oAuth2')
+    count = 100
+    tweets = api.request('statuses/user_timeline', {'screen_name': name_query, 'count':count, 'exclude_replies':'true', 'include_rts':'false'})
 
-  personalInfoResponse = api.request('users/show', {'screen_name' : name_query, 'include_entities' : 'false'}).json()
+    personalInfoResponse = api.request('users/show', {'screen_name' : name_query, 'include_entities' : 'false'}).json()
 
-  realName = personalInfoResponse['name']
-  handle = personalInfoResponse['screen_name']
-  bio = personalInfoResponse['description']
-  profilePicture = personalInfoResponse['profile_image_url']
-  coverPhoto = personalInfoResponse['profile_banner_url']
-  followers = personalInfoResponse['followers_count']
+    realName = personalInfoResponse['name']
+    handle = personalInfoResponse['screen_name']
+    bio = personalInfoResponse['description']
+    profilePicture = personalInfoResponse['profile_image_url']
+    coverPhoto = personalInfoResponse['profile_banner_url']
+    followers = personalInfoResponse['followers_count']
 
-  bio_dict = {'name' : realName, 'handle' : handle, 'bio' : bio, 'profilePicture' : profilePicture, 'coverPhoto' : coverPhoto, 'followers' : followers}
-  tweets_dict = {'tweets': []}
-  for tweet in tweets: #each tweet will be a Python dictionary
-    text = tweet['text'].encode('ascii', 'ignore')
-    text = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",text).split())
-    text.strip()
+    bio_dict = {'name' : realName, 'handle' : handle, 'bio' : bio, 'profilePicture' : profilePicture, 'coverPhoto' : coverPhoto, 'followers' : followers}
+    tweets_dict = {'tweets': []}
+    for tweet in tweets: #each tweet will be a Python dictionary
+        text = tweet['text'].encode('ascii', 'ignore')
+        text = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",text).split())
+        text.strip()
 
-    if text == "":
-      continue
+        if text == "":
+            continue
 
-    date = tweet['created_at'].encode('ascii', 'ignore')
-    retweet_count = tweet['retweet_count']
-    sentiment = vader(text)
+        date = tweet['created_at'].encode('ascii', 'ignore')
+        retweet_count = tweet['retweet_count']
+        sentiment = vader(text)
 
-    tweet_data = {"text" : text, "date" : date, "retweet_count" : retweet_count, "sentiment" : sentiment}
-    tweets_dict["tweets"].insert(0, tweet_data)
-  
-  bio_output = json.dumps(bio_dict)
-  tweets_output = json.dumps(tweets_dict)
+        tweet_data = {"text" : text, "date" : date, "retweet_count" : retweet_count, "sentiment" : sentiment}
+        tweets_dict["tweets"].insert(0, tweet_data)
 
-  context = {
-    'query': name_query
-  }
+    bio_output = json.dumps(bio_dict)
+    tweets_output = json.dumps(tweets_dict)
 
-  context = RequestContext(request, context)
-  context['bio_data'] = bio_output
-  context['tweet_data'] = tweets_output
+    context = {'query': name_query}
 
-  context['feeling_data'] = json.dumps({'January': -0.2, 'February': -0.5, 'March': 0.1, 'April':0.3, 'May': 0.2, 'June': 0.6, 'July': 0.3})
-  context['retweet_data'] = json.dumps({'January': 2341, 'February': 1232, 'March': 9083, 'April':2032, 'May': 1032, 'June': 821, 'July': 1454})
+    context = RequestContext(request, context)
+    context['bio_data'] = bio_output
+    context['tweet_data'] = tweets_output
 
-  return render_to_response('feels/display.html', context)
+    context['feeling_data'] = json.dumps({'January': -0.2, 'February': -0.5, 'March': 0.1, 'April':0.3, 'May': 0.2, 'June': 0.6, 'July': 0.3})
+    context['retweet_data'] = json.dumps({'January': 2341, 'February': 1232, 'March': 9083, 'April':2032, 'May': 1032, 'June': 821, 'July': 1454})
+
+    return render_to_response('feels/display.html', context)
