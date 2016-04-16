a011323ebe177f3b9ac24a5c4a3f1f9851ffb462
James Wall <james.wall@wustl.edu>
2016-04-16 15:35:01 +0800

    include TwitterAPI in views

--- feels/views.py
+++ feels/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from TwitterAPI import *
 
 def index(request):
     context = {}
-    # if this is a POST request we need to process the form data
+    #post request means we parse for a query
     if request.method == 'POST':
+        api = TwitterAPI('BQBZTbY3ugTypaRBq7Is0m6Dh', 'JGeRqs3r42Id4W2Q47NlGwAlNYv0myrBhlUPJeeizQXi56RWBm', auth_type='oAuth2')
         myQuery = request.POST['query'] #takes in query from front-end
-        context = { #adds query to context
-            'query': myQuery
+        r = api.request('search/tweets', {'q': myQuery})
+        '''
+        for item in r:
+            print(item)
+        '''
+        context = {  # adds query and JSON response of tweets to context
+            'query': myQuery,
+            'response': r
         }
+
     return render(request, 'feels/index.html', context)
 
 def display(request):
