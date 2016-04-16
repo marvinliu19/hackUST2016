ee3485b5bc4f793edf9ba46579220795f534d943
James Wall <james.wall@wustl.edu>
2016-04-16 15:10:56 +0800

    cleaned up query parsing and passing

--- feels/views.py
+++ feels/views.py
@@ -1,26 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import NameForm
 
 def index(request):
-  context = {}
-  # if this is a POST request we need to process the form data
-  if request.method == 'POST':
-      # create a form instance and populate it with data from the request:
-      form = NameForm(request.POST)
-      # check whether it's valid:
-      if form.is_valid():
-          myQuery = request.POST['query']
-          context = {
-              'query': myQuery
-          }
-          return render(request, 'feels/index.html', context)
-
-  # if a GET (or any other method) we'll create a blank form
-  else:
-      form = NameForm()
-
-  return render(request, 'feels/index.html', context)
+    context = {}
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        myQuery = request.POST['query'] #takes in query from front-end
+        context = { #adds query to context
+            'query': myQuery
+        }
+    return render(request, 'feels/index.html', context)
 
 def display(request):
   context = {}
