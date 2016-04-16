a9c56ba0d3b8ec1d8eb9d0209d954c56f9e162d3
Loic Turin <turin.loic@gmail.com>
2016-04-16 21:40:28 +0800

    Twitter search for one person

--- feels/twitter_tests.py
+++ feels/twitter_tests.py
@@ -6,16 +6,19 @@ import re
 api = TwitterAPI('BQBZTbY3ugTypaRBq7Is0m6Dh', 'JGeRqs3r42Id4W2Q47NlGwAlNYv0myrBhlUPJeeizQXi56RWBm', auth_type='oAuth2')
 print("authentification success\n")
 
-myQuery = "Donald Trump"
+name_query = "realdonaldtrump"
 count = 10
-r = api.request('search/tweets', {'q': myQuery, 'lang': 'en', 'count':count})
+r = api.request('statuses/user_timeline', {'screen_name': name_query, 'count':count, 'exclude_replies':'true', 'include_rts':'false'})
 
 for item in r: #each tweet will be a Python dictionary
+    
+    # print item
+
     text = item['text'].encode('ascii', 'ignore')
     print text
     
-    clean_text = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",text).split())
-    print clean_text
+    #clean_text = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",text).split())
+    # print clean_text
 
     vs = vader(text)
     
