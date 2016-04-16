1d2fcc48840f096fbc10af9e806656fc8abde7c8
James Wall <james.wall@wustl.edu>
2016-04-16 17:07:56 +0800

    twitter_test has working twitter functionality

--- feels/views.py
+++ feels/views.py
@@ -11,14 +11,20 @@ def index(request):
   return render_to_response('feels/index.html', context)
 
 def display(request):
-  context = {}
-  if request.method == "POST":
-    api = TwitterAPI('BQBZTbY3ugTypaRBq7Is0m6Dh', 'JGeRqs3r42Id4W2Q47NlGwAlNYv0myrBhlUPJeeizQXi56RWBm', auth_type='oAuth2')
-    myQuery = request.POST['query']  # takes in query from front-end
-    r = api.request('search/tweets', {'q': myQuery})
-    context = {  # adds query and JSON response of tweets to context
-        'query': myQuery,
-        'response': r
-    }
-    context = RequestContext(request, context)
-  return render_to_response('feels/display.html', context)
+    if request.method == "POST":
+        api = TwitterAPI('BQBZTbY3ugTypaRBq7Is0m6Dh', 'JGeRqs3r42Id4W2Q47NlGwAlNYv0myrBhlUPJeeizQXi56RWBm', auth_type='oAuth2')
+        myQuery = request.POST['query']  # takes in query from front-end
+        r = api.request('search/tweets', {'q': myQuery})
+        '''
+            relevant data:
+                text
+                retweet_count
+                location
+                lang (we want lang to be 'en' for the nltk to work
+        '''
+
+        context = {  # adds query and JSON response of tweets to context
+            'query': myQuery,
+        }
+        context = RequestContext(request, context)
+    return render_to_response('feels/display.html', context)
