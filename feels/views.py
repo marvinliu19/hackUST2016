9f3aef9f3de00330cbb799dfa176465e3cb2548b
James Wall <james.wall@wustl.edu>
2016-04-16 14:07:27 +0800

    working on form

--- feels/views.py
+++ feels/views.py
@@ -3,6 +3,23 @@ from django.shortcuts import render
 
 def index(request):
   context = {}
+  # if this is a POST request we need to process the form data
+  if request.method == 'POST':
+      # create a form instance and populate it with data from the request:
+      form = NameForm(request.POST)
+      # check whether it's valid:
+      if form.is_valid():
+          # process the data in form.cleaned_data as required
+          # ...
+          # redirect to a new URL:
+          cd = form.cleaned_data
+          cd
+          return HttpResponseRedirect('feels/display.html')
+
+  # if a GET (or any other method) we'll create a blank form
+  else:
+      form = NameForm()
+
   return render(request, 'feels/index.html', context)
 
 def display(request):
