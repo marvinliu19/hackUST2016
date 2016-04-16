c96608653dd99e17bd39177d05e65feab12d5d4f
Marvin Liu <marvinliu9@gmail.com>
2016-04-16 13:15:16 +0800

    add homepage template

--- feels/views.py
+++ feels/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-  return HttpResponse("Welcome to our app")
+  context = {}
+  return render(request, 'feels/index.html', context)
+
+def results(request):
+  response = "Your query was"
+  return HttpResponse(response)
--- hackust/settings.py
+++ hackust/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hackust.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['feels/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
